ffe5cd1e0fd332921529e9ce210c298aee17c44a
AntonMrO <itoxio17@mail.ru>
2024-09-19 22:23:22 +0300

    Update module_6_hard.py

--- module_6_hard.py
+++ module_6_hard.py
@@ -2,18 +2,38 @@
 # объекты которых будут обладать методами изменения размеров, цвета и т.д.
 # Многие атрибуты и методы должны быть инкапсулированны и для них должны быть написаны
 # интерфейсы взаимодействия (методы) - геттеры и сеттеры.
-from math import pi
+from math import pi, sqrt
 
 class Figure:
+    sides_count = 0
     def __init__(self, color, *sides):
-        self.sides_count = 0
-        self.__sides = sides
+        self.__sides = sides if len(sides) == self.sides_count else [1] * self.sides_count
         if self.__is_valid_color(color[0],color[1], color[2]):
             self.__color = color
         else:
-            print ( f'Некорректное значение цвета: {color}, введите заново' )
-            self.__color = ('-','-','-')
+            print ( f'Некорректное значение цвета: {color}, введите 3 значения от 0 до 255' )
+            self.__color = (255,255,255)   #устанавливает значения по-умолчанию
         self.filled = False
+
+    @staticmethod
+    def __is_valid_color(r, g, b):
+        return (0<=r<=255 and 0<=g<=255 and 0<=b<=255)
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = (r, g, b)
+        else:
+            print ( f'Некорректное значение нового цвета: {(r, g, b)}, цвет остался преждним. Введите заново...' )
+
+    @classmethod
+    def __is_valid_sides(cls, *sides):  #значение стороны "+" и целое, сравнение кол-ва с сущ. количеством сторон фигуры
+        flag = True
+        for side in sides:
+            if side <= 0 or not isinstance(side,int):
+                print(type(side))
+                flag = False
+                break
+        return flag and len(sides)==cls.sides_count
 
     def get_color(self):
         return self.__color
@@ -21,83 +41,64 @@ class Figure:
     def get_sides(self):
         return self.__sides
 
-    def __valid_c(self, a, b, c):     #проверка 1 из значений числа цвета на соответствие 0-255 и целое
-        if a in range (0, 256) and b in range (0, 256) and c in range (0, 256):
-            return True
-
-    def __is_valid_color(self, r, g, b):    #проверка RGB на соответствие параметрам с помощью метода __valid_c
-        if self.__valid_c(r, g, b):
-            return True
-        else:
-            return False
-
-    def set_color(self, r, g, b):
-        if self.__is_valid_color(r, g, b):
-            self.__color = (r, g, b)
-        # else:
-        #     print ( f'Некорректное значение цвета: {(r, g, b)}, введите заново' )
-
-    def __is_valid_sides(self, *sides):  #значение стороны "+" и целое, сравнение кол-ва с сущ. количеством сторон фигуры
-        fl = False
-        for i in sides:
-            if isinstance(i, int) and i > 0:
-                fl = True
-            else:
-                fl = False
-        if fl and len(sides) == self.sides_count:
-            return True
+    # def set_sides(self, *new_sides):
+    #     if self.__is_valid_sides(new_sides):
+    #         self.__sides = new_sides
+    #     else:
+    #         print('не корректное количество сторон или значения величины одной из сторон')
 
     def set_sides(self, *new_sides):
-        if self.__is_valid_sides(new_sides):
-            self.__sides = new_sides
+        if len(new_sides)==self.sides_count:
+            self.__sides = list(new_sides)
         else:
             print('не корректное количество сторон или значения величины одной из сторон')
 
     def __len__(self):
-        perimeter = 0
-        for i in range(len(self.__sides)):
-            perimeter += self.__sides[i-1]
-        return perimeter
+        return sum(self.__sides)
 
 class Circle(Figure):
+    sides_count = 1
     def __init__(self, color, len_sq):
-        super ().__init__ (color, len_sq)
-        self.sides_count = 1
+        super().__init__(color, len_sq)
         self.__radius = len_sq / (2 * pi)
 
     def get_square(self):
         return (self.__radius ** 2) * pi
 
 class Triangle(Figure):
-    def __init__(self, color, *side) :
-        self.sides_count = 3
-        super ().__init__ (color)
+    sides_count = 3
+    def __init__(self, color, *sides) :
+        super().__init__ (color, *sides)
 
     def get_square(self):
-        Len_2 = self.__len__() / 2
-        S = marh.sqrt(Len_2 * (Len_2 - self.__sides[0]) * (Len_2 - self.__sides[1]) * (Len_2 - self.__sides[2]))
+        p = len(self) / 2
+        S = sqrt(p * (p - self._Figure__sides[0]) * (p - self._Figure__sides[1]) * (p - self._Figure__sides[2]))
+        return S
 
 class Cube(Figure):
-    def __init__(self, color, *sides) :
-        self.sides_count = 12
-        self.__sides = sides * 12
-        super ().__init__ ( color, *sides )
+    sides_count = 12
+    def __init__(self, color, side) :
+        sides = [side] * 12
+        super().__init__(color,*sides)
 
     def get_volume(self):
-        return self.__sides[0]**3
+        return self._Figure__sides[0]**3
 
-# trig1 = Triangle((200, 200, 100), 10, 6)
-# print(trig1.get_color())
-# print(len(trig1))
-#
-# fg1 = Figure((255,0,250),12)
-# print(fg1.get_color())
-# fg1.set_color(255, 266, 77)
-# print(fg1.get_color())
-# print(len(fg1))
+# 1 треугольник
+trig1 = Triangle((222, 200, 100), 10, 6, 6)
+print(trig1.get_color())
+print(len(trig1))
+print(trig1.get_square())
 
+# 2 треугольник
+trig2 = Triangle((33, 22, 11), 10, 6) #стороны будут 1,1,1, т.к. не хватает 1 значения стороны
+print(trig2.get_color())
+print(len(trig2))
+print(trig2.get_square())
+trig2.set_color(333, 623, 72) #не изменится
+print(trig2.get_color())
 
-
+# круг
 circle1 = Circle((266, 200, 100), 10) # (Цвет, стороны)
 print(circle1.get_color())
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
@@ -120,6 +121,9 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
+
+# Проверка периметра (куба):
+print(len(cube1))
 
 
 # [55, 66, 77]
